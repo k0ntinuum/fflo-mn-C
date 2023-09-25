b0d00dc6f81292bbad16147e7da2ec4f9c568e2a
fil0s0fi <fil0s0fi@proton.me>
2023-09-25 03:55:05 -0400

    tweak

--- global.c
+++ global.c
@@ -2,19 +2,11 @@ typedef double real;
 typedef real* field;
 typedef Color color;
 
-// int R = 100; //pix rows
-// int C = 100; //pix cols
-// int H = 10; //pix height
-// int W = 10; //pix width
+int R = 100; //pix rows
+int C = 100; //pix cols
+int H = 10; //pix height
+int W = 10; //pix width
 
-// int R = 200; //pix rows
-// int C = 200; //pix cols
-// int H = 5; //pix height
-// int W = 5; //pix width
-        int R = 10; //pix rows
-        int C = 10; //pix cols
-        int H = 100; //pix height
-        int W = 100; //pix width
 const int F = 10; //max num filters
 const int L =100; //max weights in filter
 typedef struct {
